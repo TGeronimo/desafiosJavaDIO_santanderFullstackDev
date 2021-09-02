98542a6c56b9cbf7051f8eeecb9c4d459322b087
TGeronimo <thi.matheus@gmail.com>
2021-09-02 13:56:09 -0300

    Solved folha de pagamento and corrected a typo in README

--- src/FolhaDePagamento/FolhaDePagamento.java
+++ src/FolhaDePagamento/FolhaDePagamento.java
@@ -7,21 +7,21 @@ import java.util.StringTokenizer;
 
 public class FolhaDePagamento {
 
-    // CÓDIGO DE EXEMPLO DO DESAFIO NA DIO, A SER MODIFICADO
-//        public static void main(String[] args) throws IOException {
-//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//            StringTokenizer st = new StringTokenizer(br.readLine());
-//            int valor1 = Integer.parseInt(st.nextToken());
-//
-//            st = new StringTokenizer(br.readLine());
-//            int valor2 = Integer.parseInt(st.nextToken());
-//
-//            st = new StringTokenizer(br.readLine());
-//            double valor3 = Double.parseDouble(st.nextToken());
-//
-//            double salary = 1 * 1; // Altere o valor da variável com o cálculo esperado
-//            System.out.println("NUMBER = " + valor1);
-//            System.out.printf("SALARY = U$ %.2f", salary);
-//        }
-//    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int valor1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int valor2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        double valor3 = Double.parseDouble(st.nextToken());
+
+        double salary = valor2 * valor3;
+        System.out.println("NUMBER = " + valor1);
+        System.out.printf("SALARY = U$ %.2f", salary);
+    }
 }
+
