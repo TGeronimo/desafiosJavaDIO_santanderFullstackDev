c7afdae104367473474da633ca1e2562a37fdb79
TGeronimo <thi.matheus@gmail.com>
2021-09-02 13:32:49 -0300

    Solved contagem de cédulas

--- src/ContagemDeCedulas/ContagemDeCedulas.java
+++ src/ContagemDeCedulas/ContagemDeCedulas.java
@@ -14,22 +14,27 @@ public class ContagemDeCedulas{
         int atual = notasInseridas;
         int notas100 = atual / 100;
         atual -= notas100 * 100;
-        int notas50 = ;
-        atual -=  ;
-        int notas20 = ;
-        atual -=  ;
-        int notas10 =   ;
-        atual -=   ;
-        int notas5 = ;
-        atual -= ;
-        int notas2 = ;
-        atual -= ;
+        int notas50 = atual / 50;
+        atual -= notas50 * 50;
+        int notas20 = atual / 20;
+        atual -= notas20 * 20;
+        int notas10 =  atual / 10;
+        atual -=  notas10 * 10;
+        int notas5 = atual / 5;
+        atual -= notas5 * 5;
+        int notas2 = atual / 2;
+        atual -= notas2 * 2;
         int notas1 = atual;
 
         //continue a solucao de acordo com os exemplos de saída
         System.out.println(notasInseridas);
-        System.out.println(  notas100 + " nota(s) de R$ 100,00");
-        System.out.println(   + " nota(s) de R$ 50,00");
+        System.out.println(notas100 + " nota(s) de R$ 100,00");
+        System.out.println(notas50 + " nota(s) de R$ 50,00");
+        System.out.println(notas20 + " nota(s) de R$ 20,00");
+        System.out.println(notas10 + " nota(s) de R$ 10,00");
+        System.out.println(notas5 + " nota(s) de R$ 5,00");
+        System.out.println(notas2 + " nota(s) de R$ 2,00");
+        System.out.println(notas1 + " nota(s) de R$ 1,00");
     }
 
 }
--- src/FolhaDePagamento/FolhaDePagamento.java
+++ src/FolhaDePagamento/FolhaDePagamento.java
@@ -8,20 +8,20 @@ import java.util.StringTokenizer;
 public class FolhaDePagamento {
 
     // CÓDIGO DE EXEMPLO DO DESAFIO NA DIO, A SER MODIFICADO
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int valor1 = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            int valor2 = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            double valor3 = Double.parseDouble(st.nextToken());
-
-            double salary = 1 * 1; // Altere o valor da variável com o cálculo esperado
-            System.out.println("NUMBER = " + valor1);
-            System.out.printf("SALARY = U$ %.2f", salary);
-        }
-    }
+//        public static void main(String[] args) throws IOException {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            int valor1 = Integer.parseInt(st.nextToken());
+//
+//            st = new StringTokenizer(br.readLine());
+//            int valor2 = Integer.parseInt(st.nextToken());
+//
+//            st = new StringTokenizer(br.readLine());
+//            double valor3 = Double.parseDouble(st.nextToken());
+//
+//            double salary = 1 * 1; // Altere o valor da variável com o cálculo esperado
+//            System.out.println("NUMBER = " + valor1);
+//            System.out.printf("SALARY = U$ %.2f", salary);
+//        }
+//    }
 }
