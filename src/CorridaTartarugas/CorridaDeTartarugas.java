9417c1d61f7745a648b2189fdc0217c1db28c6cc
TGeronimo <thi.matheus@gmail.com>
2021-09-03 22:55:15 -0300

    New challenge tbs turtle race
    > example code pasted
    > readme file created with the challenge text

--- src/CorridaTartarugas/CorridaDeTartarugas.java
+++ src/CorridaTartarugas/CorridaDeTartarugas.java
@@ -1,4 +1,28 @@
 package CorridaTartarugas;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CorridaDeTartarugas {
+    public class Desafio {
+        public static void main(String args[]) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            //continue a solucao de acordo com o solicitado
+
+            int n;
+            do {
+                n = Integer.parseInt(br.readLine());
+                String str = br.readLine();
+                String s[] = str.split(" ");
+
+
+
+
+            }
+            System.out.println(m);
+        } while (br.ready());
+    }
+}
 }
