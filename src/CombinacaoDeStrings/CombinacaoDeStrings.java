770ff5529dad3587e6f7ffee064878d3ccba7497
TGeronimo <thi.matheus@gmail.com>
2021-09-04 20:24:30 -0300

    POSSIBLE SOLUTION

--- src/CombinacaoDeStrings/CombinacaoDeStrings.java
+++ src/CombinacaoDeStrings/CombinacaoDeStrings.java
@@ -36,3 +36,44 @@ public class CombinacaoDeStrings {
         return restoString;
     }
 }
+
+// AVALIAR ESTA SOLUÇÃO
+
+//import java.util.Scanner;
+//        import java.util.*;
+//        import java.lang.*;
+//public class Desafio {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner leitor = new Scanner(System.in);
+//
+//        int N = Integer.parseInt(leitor.nextLine());//Recebe o primeiro valor do scanner, que é o numero de cadeias
+//        int it = 0;//Será utilizado para contar iterações na hora de juntar as strings
+//        // String resultado = "";//Irá guardar o nosso resultado
+//
+//        for (int i = 0; i < N; i++) {
+//
+//            String line = leitor.nextLine();//Recebe a cadeia de strings do scanner
+//            String splitline[] = line.split(" ");//Quebra a cadeia de valores em duas partes, separadas por um espaço
+//
+//            int firstlength = splitline[0].length();//Recebe o tamanho da primeira parte
+//            int secondlength = splitline[1].length();//Recebe o tamanho da segunda parte
+//            int maxlength =  Math.max(firstlength,secondlength);//Verifica qual das strings possui o maior tamanho
+//
+//            StringBuilder construtor = new StringBuilder();//Para facilitar o processo, iremos criar um StringBuilder
+//
+//            for (int j = 0; j<maxlength; j++){//Este segundo for permite que façamos o append respeitando o tamanho dos Arrays
+//                if(j<firstlength){
+//                    construtor.append(splitline[0].charAt(j));
+//                }
+//                if(j<secondlength){
+//                    construtor.append(splitline[1].charAt(j));
+//                }
+//            }
+//
+//            String resultado = construtor.toString();//Converte o StringBuilder para String;
+//            System.out.println(resultado);//Imprime nossa saida
+//        }
+//    }
+//}
